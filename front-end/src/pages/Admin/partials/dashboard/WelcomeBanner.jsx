77548d1ab860807c0c832f538d314c67fbb93d41
yunben <vietzz771@gmail.com>
2024-06-10 16:37:21 +0700

    Fixbug ESlint

--- front-end/src/pages/Admin/partials/dashboard/WelcomeBanner.jsx
+++ front-end/src/pages/Admin/partials/dashboard/WelcomeBanner.jsx
@@ -1,9 +1,11 @@
-
 function WelcomeBanner() {
   return (
     <div className="relative bg-indigo-200 dark:bg-indigo-500 p-4 sm:p-6 rounded-sm overflow-hidden mb-8">
       {/* Background illustration */}
-      <div className="absolute right-0 top-0 -mt-4 mr-16 pointer-events-none hidden xl:block" aria-hidden="true">
+      <div
+        className="absolute right-0 top-0 -mt-4 mr-16 pointer-events-none hidden xl:block"
+        aria-hidden="true"
+      >
         <svg width="319" height="198" xmlnsXlink="http://www.w3.org/1999/xlink">
           <defs>
             <path id="welcome-a" d="M64 0l64 128-64-20-64 20z" />
@@ -46,7 +48,9 @@ function WelcomeBanner() {
 
       {/* Content */}
       <div className="relative">
-        <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">Welcome back, Admin. 👋</h1>
+        <h1 className="text-2xl md:text-3xl text-slate-800 dark:text-slate-100 font-bold mb-1">
+          Welcome back, Admin. 👋
+        </h1>
         <p className="dark:text-indigo-200">Here is what’s happening with your projects today:</p>
       </div>
     </div>
