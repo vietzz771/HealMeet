deac9d1ec455982b103a21bcab8e4dd512f2bf48
DINO7602 <Phongnbde160087@fpt.edu.vn>
2024-06-10 13:49:49 +0700

    add feature-DoctorDetail

---
+++ front-end/src/pages/Doctors/SidePanel.jsx
@@ -0,0 +1,32 @@
+import convertTime from '../../utils/convertTime';
+
+const SidePanel = ({ doctorId, ticketPrice, timeSlots }) => {
+  return (
+    <div className="shadow-[0_35px_60px_-15px_rgba(255,255,255,0.3)] shadow-gray-400 p-3 lg:p-5 rounded-md">
+      <div className="flex items-center justify-between">
+        <p className="text__para mt-0 font-semibold">Ticket Price</p>
+        <span className="text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold">
+          {ticketPrice} $
+        </span>
+      </div>
+      <div className="mt-[30px]">
+        <p className="text__para mt-0 font-semibold text-headingColor">Available Time Slots:</p>
+        <ul className="mt-3">
+          {timeSlots?.map((item, index) => (
+            <li key={index} className="flex items-center justify-between mb-2">
+              <p className="text-[15px] leading-6 text-textColor font-semibold capitalize">
+                {item.day}
+              </p>
+              <p className="text-[15px] leading-6 text-textColor font-semibold">
+                {convertTime(item.startingTime)} - {convertTime(item.endingTime)}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className="btn px-2 w-full rounded-md">Book Appointment</button>
+    </div>
+  );
+};
+
+export default SidePanel;
