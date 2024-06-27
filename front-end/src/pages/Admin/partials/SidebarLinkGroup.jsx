029be962ed5091af3f32d500f7d96b05cf46630d
The Hoang <thehoang1722@gmail.com>
2024-06-27 15:26:54 +0700

    fix Sidebar

--- front-end/src/pages/Admin/partials/SidebarLinkGroup.jsx
+++ front-end/src/pages/Admin/partials/SidebarLinkGroup.jsx
@@ -9,7 +9,7 @@ function SidebarLinkGroup({ children, activecondition }) {
   };
 
   return (
-    <li className={`px-3 py-2 rounded-sm mb-0.5 last:mb-0 ${activecondition && 'bg-slate-900'}`}>
+    <li className={`px-3 py-3 rounded-sm mb-0.5 last:mb-0 ${activecondition && 'bg-active'}`}>
       {children(handleClick, open)}
     </li>
   );
--- front-end/src/pages/Admin/partials/dashboard/WelcomeBanner.jsx
+++ front-end/src/pages/Admin/partials/dashboard/WelcomeBanner.jsx
@@ -5,13 +5,17 @@ function WelcomeBanner() {
       style={{ backgroundImage: "url('/public/images/welcomeBanner.png')" }}
     >
       <div className="text-white w-1/2">
-        <h2 className="text-2xl font-semibold">Welcome back, Admin. 👋</h2>
+        <h2 className="text-xl lg:text-2xl font-semibold">Welcome back, Admin. 👋</h2>
         <br />
         <div className="border-b border-gray-300 my-2"></div>
-        At HealMeet, we believe that every patient deserves the highest quality care possible.
+        <p className="text-sm lg:text-base">
+          At HealMeet, we believe that every patient deserves the highest quality care possible.
+        </p>
         <br />
-        Our commitment to excellence in healthcare is matched only by our compassion for those we
-        serve.
+        <p className="text-sm lg:text-base">
+          Our commitment to excellence in healthcare is matched only by our compassion for those we
+          serve.
+        </p>
       </div>
     </div>
   );
